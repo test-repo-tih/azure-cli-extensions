055ef38cbf66733d900b758ca0e91cc7fe4f9464
Zim Kalinowski <zikalino@microsoft.com>
2019-10-08 12:40:10 +0800

    several naming fixes to backpropagate to generator

--- src/peering/azext_peering/commands.py
+++ src/peering/azext_peering/commands.py
@@ -27,31 +27,31 @@ def load_command_table(self, _):
     peering_peerings = CliCommandType(
         operations_tmpl='azext_peering.vendored_sdks.peering.operations._peerings_operations#PeeringsOperations.{}',
         client_factory=cf_peerings)
-    with self.command_group('peering', peering_peerings, client_factory=cf_peerings) as g:
-        g.custom_command('create', 'create_peering')
-        g.generic_update_command('update', custom_func_name='update_peering')
+    with self.command_group('peering asn', peering_peerings, client_factory=cf_peerings) as g:
+        g.custom_command('create', 'create_peering asn')
+        g.generic_update_command('update', custom_func_name='update_peering_asn')
         g.command('delete', 'delete')
-        g.custom_command('list', 'list_peering')
+        g.custom_command('list', 'list_peering_asn')
         g.show_command('show', 'get')
 
     from ._client_factory import cf_prefixes
     peering_prefixes = CliCommandType(
         operations_tmpl='azext_healthcare.vendored_sdks.peering.operations._prefixes_operations#PrefixesOperations.{}',
         client_factory=cf_prefixes)
-    with self.command_group('peering prefixe', peering_prefixes, client_factory=cf_prefixes) as g:
-        g.custom_command('create', 'create_peering_prefixe')
-        g.generic_update_command('update', custom_func_name='update_peering_prefixe')
+    with self.command_group('peering prefix', peering_prefixes, client_factory=cf_prefixes) as g:
+        g.custom_command('create', 'create_peering_prefix')
+        g.generic_update_command('update', custom_func_name='update_peering_prefix')
         g.command('delete', 'delete')
-        g.custom_command('list', 'list_peering_prefixe')
+        g.custom_command('list', 'list_peering_prefix')
         g.show_command('show', 'get')
 
     from ._client_factory import cf_peering_services
     peering_peering_services = CliCommandType(
         operations_tmpl='azext_peering.vendored_sdks.peering.operations._peering_services_operations#PeeringServicesOperations.{}',
         client_factory=cf_peering_services)
-    with self.command_group('peering', peering_peering_services, client_factory=cf_peering_services) as g:
-        g.custom_command('create', 'create_peering')
-        g.generic_update_command('update', custom_func_name='update_peering')
+    with self.command_group('peering service', peering_peering_services, client_factory=cf_peering_services) as g:
+        g.custom_command('create', 'create_peering_service')
+        g.generic_update_command('update', custom_func_name='update_peering_service')
         g.command('delete', 'delete')
-        g.custom_command('list', 'list_peering')
+        g.custom_command('list', 'list_peering_service')
         g.show_command('show', 'get')
--- src/peering/azext_peering/custom.py
+++ src/peering/azext_peering/custom.py
@@ -11,25 +11,25 @@
 import json
 
 
-def create_peering(cmd, client,
-                   name,
-                   peer_asn=None,
-                   peer_contact_info=None,
-                   peer_name=None,
-                   validation_state=None):
+def create_peering_asn(cmd, client,
+                       name,
+                       peer_asn=None,
+                       peer_contact_info=None,
+                       peer_name=None,
+                       validation_state=None):
     return client.create_or_update(peer_asn_name=name, peer_asn=peer_asn)
 
 
-def update_peering(cmd, client, body,
-                   name,
-                   peer_asn=None,
-                   peer_contact_info=None,
-                   peer_name=None,
-                   validation_state=None):
+def update_peering_asn(cmd, client, body,
+                       name,
+                       peer_asn=None,
+                       peer_contact_info=None,
+                       peer_name=None,
+                       validation_state=None):
     return client.create_or_update(peer_asn_name=name, peer_asn=peer_asn)
 
 
-def list_peering(cmd, client):
+def list_peering_asn(cmd, client):
     return client.list_by_subscription()
 
 
@@ -72,50 +72,50 @@ def list_peering(cmd, client,
     return client.list_by_subscription()
 
 
-def create_peering_prefixe(cmd, client,
-                           resource_group,
-                           peering_service_name,
-                           name,
-                           prefix=None):
+def create_peering_prefix(cmd, client,
+                          resource_group,
+                          peering_service_name,
+                          name,
+                          prefix=None):
     return client.create_or_update(resource_group_name=resource_group, peering_service_name=peering_service_name, prefix_name=name)
 
 
-def update_peering_prefixe(cmd, client, body,
-                           resource_group,
-                           peering_service_name,
-                           name,
-                           prefix=None):
+def update_peering_prefix(cmd, client, body,
+                          resource_group,
+                          peering_service_name,
+                          name,
+                          prefix=None):
     return client.create_or_update(resource_group_name=resource_group, peering_service_name=peering_service_name, prefix_name=name)
 
 
-def list_peering_prefixe(cmd, client,
-                         resource_group,
-                         peering_service_name):
+def list_peering_prefix(cmd, client,
+                        resource_group,
+                        peering_service_name):
     return client.list_by_peering_service(resource_group_name=resource_group, peering_service_name=peering_service_name)
 
 
-def create_peering(cmd, client,
-                   resource_group,
-                   name,
-                   location,
-                   peering_service_location=None,
-                   peering_service_provider=None,
-                   tags=None):
+def create_peering_service(cmd, client,
+                           resource_group,
+                           name,
+                           location,
+                           peering_service_location=None,
+                           peering_service_provider=None,
+                           tags=None):
     return client.create_or_update(resource_group_name=resource_group, peering_service_name=name)
 
 
-def update_peering(cmd, client, body,
-                   resource_group,
-                   name,
-                   location,
-                   peering_service_location=None,
-                   peering_service_provider=None,
-                   tags=None):
+def update_peering_service(cmd, client, body,
+                           resource_group,
+                           name,
+                           location,
+                           peering_service_location=None,
+                           peering_service_provider=None,
+                           tags=None):
     return client.create_or_update(resource_group_name=resource_group, peering_service_name=name)
 
 
-def list_peering(cmd, client,
-                 resource_group):
+def list_peering_service(cmd, client,
+                         resource_group):
     if resource_group is not None:
         return client.list_by_resource_group(resource_group_name=resource_group)
     return client.list_by_subscription()
