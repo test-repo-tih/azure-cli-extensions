28d349e374c3d7e26c700985f6e56bf7cf2e9371
Zim Kalinowski <zikalino@microsoft.com>
2019-10-08 12:27:07 +0800

    fixed paths

--- src/peering/azext_peering/commands.py
+++ src/peering/azext_peering/commands.py
@@ -14,7 +14,7 @@ def load_command_table(self, _):
 
     from ._client_factory import cf_peer_asns
     peering_peer_asns = CliCommandType(
-        operations_tmpl='azure.mgmt.peering.operations.peer_asns_operations#PeerAsnsOperations.{}',
+        operations_tmpl='azext_peering.vendored_sdks.peering.operations.peer_asns_operations#PeerAsnsOperations.{}',
         client_factory=cf_peer_asns)
     with self.command_group('peering', peering_peer_asns, client_factory=cf_peer_asns) as g:
         g.custom_command('create', 'create_peering')
@@ -25,7 +25,7 @@ def load_command_table(self, _):
 
     from ._client_factory import cf_peerings
     peering_peerings = CliCommandType(
-        operations_tmpl='azure.mgmt.peering.operations.peerings_operations#PeeringsOperations.{}',
+        operations_tmpl='azext_peering.vendored_sdks.peering.operations.peerings_operations#PeeringsOperations.{}',
         client_factory=cf_peerings)
     with self.command_group('peering', peering_peerings, client_factory=cf_peerings) as g:
         g.custom_command('create', 'create_peering')
@@ -36,7 +36,7 @@ def load_command_table(self, _):
 
     from ._client_factory import cf_prefixes
     peering_prefixes = CliCommandType(
-        operations_tmpl='azure.mgmt.peering.operations.prefixes_operations#PrefixesOperations.{}',
+        operations_tmpl='azext_healthcare.vendored_sdks.peering.operations.prefixes_operations#PrefixesOperations.{}',
         client_factory=cf_prefixes)
     with self.command_group('peering prefixe', peering_prefixes, client_factory=cf_prefixes) as g:
         g.custom_command('create', 'create_peering_prefixe')
@@ -47,7 +47,7 @@ def load_command_table(self, _):
 
     from ._client_factory import cf_peering_services
     peering_peering_services = CliCommandType(
-        operations_tmpl='azure.mgmt.peering.operations.peering_services_operations#PeeringServicesOperations.{}',
+        operations_tmpl='azext_peering.vendored_sdks.peering_services_operations#PeeringServicesOperations.{}',
         client_factory=cf_peering_services)
     with self.command_group('peering', peering_peering_services, client_factory=cf_peering_services) as g:
         g.custom_command('create', 'create_peering')
